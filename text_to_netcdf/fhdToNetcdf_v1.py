f91db703c0d94ee43e81b621abcf730bf0c4b3b4
lisapalathingal <lisa.palathingal@gmail.com>
2016-01-06 13:00:04 -0800

    Added modflow files

---
+++ text_to_netcdf/fhdToNetcdf_v1.py
@@ -0,0 +1,213 @@
+import netCDF4
+import osr
+import sys
+
+def find_time_unit(timeValue):
+	
+	if timeValue == 0:
+		timeUnit = "undefined"
+	elif timeValue == 1:
+		timeUnit = "seconds"
+	elif timeValue == 2:
+		timeUnit = "minutes"
+	elif timeValue == 3:
+		timeUnit = "hours"
+	elif timeValue == 4:
+		timeUnit = "days"
+	elif timeValue == 5:
+		timeUnit = "years"
+	return timeUnit			
+
+def find_length_unit(lengthValue):
+	
+	if lengthValue == 0:
+		lengthUnit = "undefined"
+	elif lengthValue == 1:
+		lengthUnit = "feet"
+	elif lengthValue == 2:
+		lengthUnit = "meters"
+	elif lengthValue == 3:
+		lengthUnit = "centimeters"
+	return lengthUnit			
+
+def find_values(fileHandle, numberOfHRUs, stressPeriodIndex, timeStepIndex, layerIndex):
+	
+	hruValues = [] 
+	sum = 0
+	
+	for line in fileHandle:
+		if "HEAD" in line:
+			indexValues = line.strip().split()
+			if int(indexValues[0]) == timeStepIndex and int(indexValues[1]) == stressPeriodIndex and int(indexValues[7]) == layerIndex:
+				while sum != numberOfHRUs:
+					values = fileHandle.next().split()
+					lengthOfLine = len(values)
+					sum += lengthOfLine
+					for index in range(lengthOfLine):
+						hruValues.append(float(values[index]))
+	
+	return hruValues
+
+
+def find_average_resolution(fileHandle, numberOfHRUs, numberOfRows, numberOfColumns):
+
+    latitudeValues = []
+    longitudeValues = []
+   
+    for i in range(numberOfHRUs):
+    	valuesInLine = fileHandle.next().strip().split()
+    	longitudeValues.append(float(valuesInLine[1]))
+    	latitudeValues.append(float(valuesInLine[2]))
+
+    minimumLatitudeValue = min(latitudeValues)
+    maximumLatitudeValue = max(latitudeValues)
+
+    minimumLongitudeValue = min(longitudeValues)
+    maximumLongitudeValue = max(longitudeValues)
+
+    averageOfLatitudeValues = (maximumLatitudeValue-minimumLatitudeValue)/numberOfRows
+    averageOfLongitudeValues = (maximumLongitudeValue-minimumLongitudeValue)/numberOfColumns
+     
+    latitudeOfFirstHru = latitudeValues[0]
+    longitudeOfFirstHru = longitudeValues[0]
+
+    return averageOfLatitudeValues, averageOfLongitudeValues, latitudeOfFirstHru, longitudeOfFirstHru
+	
+
+def fhd_to_netcdf(fhdFile, disFile, locationFile, fileOutput):
+
+	totalNumberOfTimeSteps = 0
+
+	fileHandle = open(disFile, 'r')
+	for line in fileHandle:
+		if "#" not in line.strip().split()[0]:
+			values = line.split()
+			break
+
+	numberOfLayers = int(values[0])
+	numberOfRows = int(values[1])
+	numberOfColumns = int(values[2])
+	numberOfHRUs = numberOfRows * numberOfColumns
+	numberOfStressPeriods = int(values[3])
+	timeValue = int(values[4])
+	timeUnit = find_time_unit(timeValue)
+	lengthValue = int(values[5])
+	lengthUnit = find_length_unit(lengthValue)
+	
+	stressPeriodLengths = []
+	timeStepCounts = []
+	multiplierCounts = []
+	stressPeriodStates = []
+	
+	for index in range(numberOfStressPeriods, 0, -1):
+		fileHandle = open(disFile, 'r')
+		lineNumber = index * -1
+		values = fileHandle.readlines()[lineNumber].split()
+		lengthOfStressPeriod = float(values[0])
+		numberOfTimeSteps = int(values[1])
+		multiplier = float(values[2])
+		stateOfStressPeriod = values[3]
+
+		stressPeriodLengths.append(lengthOfStressPeriod)
+		timeStepCounts.append(numberOfTimeSteps)
+		multiplierCounts.append(multiplier)
+		stressPeriodStates.append(stateOfStressPeriod)
+
+
+	for index in range(len(timeStepCounts)):
+		totalNumberOfTimeSteps += timeStepCounts[index]
+
+	layerIndices = []
+	timeStepIndices = []
+	stressPeriodIndices = []
+	stressPeriodTimeIndices = []
+	simulationTimeIndices = []
+
+	fileHandle = open(fhdFile, 'r')
+	for line in fileHandle:
+		if "HEAD" in line:
+			values = line.strip().split()
+			timeStepIndex = int(values[0])
+			stressPeriodIndex = int(values[1])
+			timeIndexOfStressPeriod = float(values[2])
+			timeIndexOfSimulation = float(values[3])
+
+			timeStepIndices.append(timeStepIndex)
+			stressPeriodIndices.append(stressPeriodIndex)
+			stressPeriodTimeIndices.append(timeIndexOfStressPeriod)
+			simulationTimeIndices.append(timeIndexOfSimulation)
+
+	value = len(timeStepIndices) / numberOfLayers
+	for i in range(value):
+		for j in range(1, numberOfLayers+1):
+			layerIndices.append(j)
+	
+	fileHandle = open(locationFile, 'r')
+	values = find_average_resolution(fileHandle, numberOfHRUs, numberOfRows, numberOfColumns)
+	averageOfLatitudeValues = values[0]
+	averageOfLongitudeValues = values[1]
+	latitudeOfFirstHru = values[2]
+	longitudeOfFirstHru = values[3]
+
+	# Initialize new dataset
+	ncfile = netCDF4.Dataset('fhd.nc', mode='w')
+
+	# Initialize dimensions
+	lat_dim = ncfile.createDimension('lat', numberOfRows)
+	lon_dim = ncfile.createDimension('lon', numberOfColumns)
+
+	latList = []
+	latList.append(latitudeOfFirstHru)
+	previousValue = latitudeOfFirstHru
+	lat = ncfile.createVariable('lat', 'f8', ('lat',))
+	lat.long_name = 'latitude'  
+	lat.units = 'degrees_north'
+	for i in range(numberOfRows - 1):
+		newValue = previousValue - averageOfLatitudeValues
+		latList.append(newValue)
+		previousValue = newValue
+	lat[:] = latList
+
+	lonList = []
+	lonList.append(longitudeOfFirstHru)
+	previousValue = longitudeOfFirstHru
+	lon = ncfile.createVariable('lon', 'f8', ('lon',))
+	lon.long_name = 'longitude'  
+	lon.units = 'degrees_east'
+	for i in range(numberOfColumns - 1):
+		newValue = previousValue + averageOfLongitudeValues
+		lonList.append(newValue)
+		previousValue = newValue
+	lon[:] = lonList
+
+	sr = osr.SpatialReference()
+	sr.ImportFromEPSG(4326)
+	crs = ncfile.createVariable('crs', 'S1',)
+	crs.spatial_ref = sr.ExportToWkt()
+	
+	for index in range(len(timeStepIndices)):
+		var = ncfile.createVariable("head_"+str(stressPeriodIndices[index])+"_"+str(timeStepIndices[index])+"_"+str(layerIndices[index]), 'f8', ('lat', 'lon'))
+		var.name = "head_" +str(stressPeriodIndices[index])+ "_" +str(timeStepIndices[index])
+		var.description = "Water head for HRUs in Stress Period " +str(stressPeriodIndices[index])+ " Time Step " +str(timeStepIndices[index])+ " Layer " +str(layerIndices[index])
+		var.units = lengthUnit
+		fileHandle = open(fhdFile, 'r')
+		values = find_values(fileHandle, numberOfHRUs, stressPeriodIndices[index], timeStepIndices[index], layerIndices[index])
+		var[:] = values
+
+	
+if __name__ == "__main__":
+
+	numberOfArgs = len(sys.argv)
+
+	for index in range(numberOfArgs):
+
+		if sys.argv[index] == "-fhd":
+			fhdFile = sys.argv[index+1]
+		elif sys.argv[index] == "-dis":
+			disFile = sys.argv[index+1]
+		elif sys.argv[index] == "-loc":
+			locationFile = sys.argv[index+1]
+
+	fhd_to_netcdf(fhdFile, disFile, locationFile, 'fhd.nc')
+    
+    
